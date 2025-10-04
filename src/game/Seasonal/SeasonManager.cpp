73f5f242acccd74d2f1656e5e4c5e005488ead39
Ryan Morris <morrisman_3@hotmail.com>
2025-10-04 14:27:58 -0600

    start of seasonal stats

---
+++ src/game/Seasonal/SeasonManager.cpp
@@ -0,0 +1,6 @@
+#include "Seasonal/SeasonManager.h"
+#include <cstdarg>
+
+INSTANTIATE_SINGLETON_1(SeasonManager);
+
+
---
+++ src/game/Seasonal/SeasonManager.h
@@ -0,0 +1,27 @@
+#ifndef __ScalingManager_H
+#define __ScalingManager_H
+
+#include "Common.h"
+#include "Policies/Singleton.h"
+
+struct SeasonCharacterStats
+{
+    float attackSpeedBonus {0.0f};
+    float castSpeedBonus {0.0f};
+    float damageBonus {0.0f};
+    float damageReductionBonus {0.0f};
+    float moveSpeedBonus {0.0f};
+};
+
+class SeasonManager
+{
+public:
+    SeasonManager() = default;
+    ~SeasonManager() {};
+
+private:
+    std::unordered_map<uint32, SeasonCharacterStats> characterStatsMap;
+};
+
+#define sSeasonManager MaNGOS::Singleton<SeasonManager>::Instance()
+#endif
